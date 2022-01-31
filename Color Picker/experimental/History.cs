c4d4e2301a6aebcb91ee8d10c319628aa6f3d765
Jason Pezzimenti <jasonpezzimenti@hotmail.com>
2022-02-01 10:56:56 +1100

    experimenting

---
+++ Color Picker/experimental/History.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Color_Picker.experimental
+{
+    public class History
+    {
+        [DataMember]
+        public static List<ColorPallette> Pallettes;
+
+        static History()
+        {
+            History.Pallettes = new List<ColorPallette>();
+        }
+
+        public static bool Exists(string filePath) { return false; }
+        public static void Load(string filePath) { }
+        public static void Delete(string filePath) { }
+        public static void Save(string filePath) { }
+    }
+}
